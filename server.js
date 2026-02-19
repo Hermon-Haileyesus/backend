d99857f9b093c6fa604a6481d5f88aa17bb37ee2
Hermon <hg2397@edu.turku.fi>
2026-02-19 20:38:24 +0200

    add dockerfile

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ app.use(bodyParser.json());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: "localhost",
-
-  user: "Hermon",
-  password: "IloveJesus",
-  database: "project",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
